33d4d334f737ed953084363b0d6f612bf61de000
Dominic Walsh <djcwalsh94@gmail.com>
2022-12-01 17:27:32 +0200

    day 1 done - refactored

--- day_1.rb
+++ day_1.rb
@@ -1,9 +1,9 @@
-data = File.foreach("day_1_input.txt", "\n\n").to_a
+data = File.foreach('day_1_input.txt', "\n\n").to_a
 
 summed_groups = data.map do |group|
   group.split("\n").map(&:to_i).sum
 end
 
-top_three_total = summed_groups.sort.reverse.first(3).sum
+top_three_total = summed_groups.max(3).sum
 
 puts top_three_total
