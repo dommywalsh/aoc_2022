13cb739140a22a70caf4b307caa1a88085b465b7
Dominic Walsh <djcwalsh94@gmail.com>
2022-12-01 17:02:00 +0200

    completed bruuuu

--- day_1.rb
+++ day_1.rb
@@ -1,3 +1,8 @@
-File.open("day_1_input.txt").each do |line|
-  p line
+data = File.foreach("day_1_input.txt", "\n\n").to_a
+# take all the data and split into an array in groups that are separated by spaces
+
+summed_groups = data.map do |group|
+  group.split("\n").map(&:to_i).sum
 end
+
+puts summed_groups.max
