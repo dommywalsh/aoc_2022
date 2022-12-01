fd5c272c9a428608d15bbf93c8f70f8b19a6e720
Dominic Walsh <djcwalsh94@gmail.com>
2022-12-01 17:17:38 +0200

    day 1 done

--- day_1.rb
+++ day_1.rb
@@ -1,8 +1,9 @@
 data = File.foreach("day_1_input.txt", "\n\n").to_a
-# take all the data and split into an array in groups that are separated by spaces
 
 summed_groups = data.map do |group|
   group.split("\n").map(&:to_i).sum
 end
 
-puts summed_groups.max
+top_three_total = summed_groups.sort.reverse.first(3).sum
+
+puts top_three_total
