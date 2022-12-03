60238142ed8f247ba00f470203a49b3822f968c7
Dominic Walsh <djcwalsh94@gmail.com>
2022-12-03 21:02:35 +0200

    chit

--- day_3/day_3.rb
+++ day_3/day_3.rb
@@ -1,3 +1,4 @@
+# part 1
 alphabet = ('a'..'z').to_a + ('A'..'Z').to_a
 
 bags = File.open('day_3_input.txt').each_line.map { |line| line.chomp.chars }
@@ -14,6 +15,18 @@ common_letters.flatten.each do |letter|
   sum += alphabet.find_index(letter) + 1
 end
 
-#part 2
+# part 2
 
+data = File.read('day_3_input.txt')
 
+groups = data.split.each_slice(3).to_a
+
+p groups
+
+groups.
+
+# badge = groups.each do |group|
+#   group[0] & group[1] & group[2]
+# end
+
+# p badge
